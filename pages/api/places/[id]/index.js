1b294ded236b3bd47106478ea4c10124385a4bb1
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-05-09 13:58:43 +0200

    fully function

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -13,4 +13,16 @@ export default async function handler(request, response) {
     }
     response.status(200).json(place);
   }
+
+  if (request.method === "PATCH") {
+    const placeToUpdate = await Place.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+    response.status(200).json(placeToUpdate);
+  }
+
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+    response.status(200).json({ success: true });
+  }
 }
