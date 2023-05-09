15f69ed51f5c863e6bb12e96af835ba453494826
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-05-09 10:33:39 +0200

    added GET request

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -4,4 +4,13 @@ import Place from "../../../../db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
+
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+
+    if (!place) {
+      return response.status(404).json({ success: false });
+    }
+    response.status(200).json(place);
+  }
 }
