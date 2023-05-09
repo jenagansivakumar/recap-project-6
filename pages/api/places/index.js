d089b3b61d2f76dd7dac1b1863faf454d7dc6bc5
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-05-09 10:20:00 +0200

    added connect.js

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,17 +1,5 @@
 import dbConnect from "../../../db/connect";
-import { places } from '../../../lib/db';
-
-export default function handler(request, response) {
-    await dbConnect();
-    if (request.method === "GET") {
-    const places = await Place.find();
-    return response.status(200).json(places);
-  }
-
-  return response.status(200).json(places);
-}
-
-
+import { places } from "../../../lib/db";
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -20,18 +8,17 @@ export default async function handler(request, response) {
     const places = await Place.find();
     return response.status(200).json(places);
   }
+
   if (request.method === "POST") {
     try {
-      const jokeData = request.body 
-      // create a new joke document
-      // jokeData has to be in the format that is
-      // specified in the Joke model
-      const joke = new Joke(jokeData)
-      await joke.save()
-      return response.status(201).json(joke);
-    } catch(error) {
-      console.log(error)
-      return response.status(400).json({error: error.message});
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json(place);
+    } catch (error) {
+      console.log(error);
+
+      return response.status(200).json(places);
     }
   }
 }
