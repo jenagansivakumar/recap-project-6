b98f751fd58045842f87e6ae63a7511520efa936
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-05-09 11:26:42 +0200

    things are rendering

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -9,7 +9,7 @@ export default async function handler(request, response) {
     const place = await Place.findById(id);
 
     if (!place) {
-      return response.status(404).json({ success: false });
+      return response.status(404).json("Place not found");
     }
     response.status(200).json(place);
   }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -6,19 +6,19 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const places = await Place.find();
+    console.log("places:", places);
     return response.status(200).json(places);
-  }
 
-  if (request.method === "POST") {
-    try {
-      const placeData = request.body;
-      const place = new Place(placeData);
-      await place.save();
-      return response.status(201).json(place);
-    } catch (error) {
-      console.log(error);
-
-      return response.status(200).json(places);
-    }
+    // if (request.method === "POST") {
+    //   try {
+    //     const placeData = request.body;
+    //     const place = new Place(placeData);
+    //     await place.save();
+    //     return response.status(201).json(place);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // return response.status(200).json(places);
   }
 }
